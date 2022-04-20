cc72dcc28ef8b6637974266d8d96e4968817773b
Lance Cruzado <raphaellcruzado@gmail.com>
2022-04-20 05:44:31 +0000

    laout-icon

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// <div class="ingredients">
+//   <ul>
+//     <li>1 cups Barley</li>
+//     <li>3 tbsp Kosher Salt</li>
+//     <li>11 liters water</li>
+//   </ul>
+//   <div>
+//     <button id="submit-btn">Let's Cook!</button>
+//   </div>
+// </div>
+
+// get edditing pen ssymbol
+// get Recipe Title to append to page after user clicks away or presses enter
+// get URL to post something on the picture placeholder
+// create DOM tree for ingredients, once user completes ingredients prompt
+// submit button creates an index for the current ingredient list
